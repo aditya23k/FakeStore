a4d448e30de9f7e2b0ea38052ec1d66891a150b2
aditya4mk <aditya.28.1@mountblue.tech>
2025-09-30 19:29:30 +0530

    created DTO

---
+++ src/main/java/com/example/FakeStore/dtos/fakeStoreDTO.java
@@ -0,0 +1,17 @@
+package com.example.FakeStore.dtos;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+public class fakeStoreDTO{
+    private Long id;
+    private String title;
+    private double price;
+    private String category;
+    private String description;
+    private String image;
+
+
+
+}
