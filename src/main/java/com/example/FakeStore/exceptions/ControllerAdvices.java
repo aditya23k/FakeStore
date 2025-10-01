79e0c5a1c1b21297759d864761517b9f6fb33005
aditya4mk <aditya.28.1@mountblue.tech>
2025-10-02 01:31:12 +0530

    created an exceptions package with a NotFoundException and Controller Advices classes

--- src/main/java/com/example/FakeStore/exceptions/ControllerAdvices.java
+++ src/main/java/com/example/FakeStore/exceptions/ControllerAdvices.java
@@ -1,2 +1,16 @@
-package com.example.FakeStore.exceptions;public class ControllerAdvices {
+package com.example.FakeStore.exceptions;
+
+import com.example.FakeStore.dtos.ExceptionDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ControllerAdvices {
+
+    @ExceptionHandler(NotFoundException.class)
+    private ResponseEntity<ExceptionDTO> handleNotFoundException(NotFoundException notFoundException){
+        return new ResponseEntity(new ExceptionDTO(HttpStatus.NOT_FOUND, notFoundException.getMessage()),HttpStatus.NOT_FOUND);
+    }
 }
---
+++ src/main/java/com/example/FakeStore/exceptions/NotFoundException.java
@@ -0,0 +1,7 @@
+package com.example.FakeStore.exceptions;
+
+public class NotFoundException extends Exception{
+    public NotFoundException(String message){
+        super(message);
+    }
+}
