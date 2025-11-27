86e617c86df8d6bdae32fc649a3681f71b13ffc9
aditya4mk <aditya.28.1@mountblue.tech>
2025-11-27 16:52:44 +0530

    created a builder class for Product

--- src/main/java/com/example/FakeStore/models/Product.java
+++ src/main/java/com/example/FakeStore/models/Product.java
@@ -1,10 +1,12 @@
 package com.example.FakeStore.models ;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Builder
 public class Product extends BaseModel{
     public Long id;
     public String title;
@@ -13,5 +15,60 @@ public class Product extends BaseModel{
     public Category category;
     public double price;
 
+
+    public Product(String title, String description, String image, Category category, double price) {
+        this.title = title;
+        this.description = description;
+        this.image = image;
+        this.category = category;
+        this.price = price;
+    }
+
+    public Product() {
+    }
+
+    public static ProductBuilder builder() {
+        return new ProductBuilder();
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public String getImage() {
+        return this.image;
+    }
+
+    public Category getCategory() {
+        return this.category;
+    }
+
+    public double getPrice() {
+        return this.price;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
 
 }
