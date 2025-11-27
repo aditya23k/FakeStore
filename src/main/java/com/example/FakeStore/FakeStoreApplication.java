feebc5b897509c8b34935d7dc4b5e5938071994d
aditya4mk <aditya.28.1@mountblue.tech>
2025-11-27 23:54:20 +0530

    Modified the original application file to create the category and the products belonging to that category using the respective Jpa Repositories

--- src/main/java/com/example/FakeStore/FakeStoreApplication.java
+++ src/main/java/com/example/FakeStore/FakeStoreApplication.java
@@ -1,13 +1,47 @@
 package com.example.FakeStore;
 
+import java.util.*;
+import com.example.FakeStore.models.Category;
+import com.example.FakeStore.models.Product;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import com.example.FakeStore.repositories.ProductRepository;
+import com.example.FakeStore.repositories.CategoryRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FakeStoreApplication {
+public class FakeStoreApplication implements CommandLineRunner {
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(FakeStoreApplication.class, args);
 	}
 
+	private final ProductRepository productRepository;
+	private final CategoryRepository categoryRepository;
+
+
+	public FakeStoreApplication(ProductRepository productRepository, CategoryRepository categoryRepository){
+		this.categoryRepository = categoryRepository;
+		this.productRepository = productRepository;
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Category category = new Category();
+		category.setName("Apple Devices");
+		Category savedCategory = categoryRepository.save(category);
+
+		Product product = new Product();
+		product.setTitle("iPhone 15 Pro");
+		product.setDescription("The best iPhone Ever");
+		product.setCategory(savedCategory);
+
+		productRepository.save(product);
+
+		Category category1 = categoryRepository.findById(UUID.fromString("5e6f679e-f326-44ae-b220-544b3822ab00")).get();
+		System.out.println("Category name is: " + category1.getName());
+		System.out.println("Printing all products in the category");
+		Thread.sleep(1000);
+	}
 }
