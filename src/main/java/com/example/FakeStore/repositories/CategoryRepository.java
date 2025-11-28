a7e7432822ca0cac5d5b9218f47e6ee2d284bb97
aditya4mk <aditya.28.1@mountblue.tech>
2025-11-28 17:48:37 +0530

    made a Lazy initialization

--- src/main/java/com/example/FakeStore/repositories/CategoryRepository.java
+++ src/main/java/com/example/FakeStore/repositories/CategoryRepository.java
@@ -1,12 +1,14 @@
 package com.example.FakeStore.repositories;
 
 import com.example.FakeStore.models.Category;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
 @Repository
+@Lazy
 public interface CategoryRepository extends JpaRepository<Category, UUID> {
 
 }
